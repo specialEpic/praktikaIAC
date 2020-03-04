d3a0a8b9e0470a4296115b28c4566ffb39885597
mikhail <mihan.yegorov@gmail.com>
2020-03-04 23:48:03 +0300

    1.1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Главная страница</title>
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<div class="main">
+    <div class="bar">
+        <img src="logo.img" alt="logotip" class="logo">
+
+    </div>
+    <div class="login">
+        <h1></h1>
+
+    </div>
+</div>
+</body>
+</html>
