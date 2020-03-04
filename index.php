5053500a0cd5c114dde564c901585710ee806c34
mikhail <mihan.yegorov@gmail.com>
2020-03-05 01:18:31 +0300

    1.2

--- index.php
+++ index.php
@@ -9,12 +9,51 @@
 <div class="main">
     <div class="bar">
         <img src="logo.img" alt="logotip" class="logo">
-
     </div>
     <div class="login">
-        <h1></h1>
-
+        <h1>Для доступа на портал обратной связи пройдите авторизацию</h1>
+        <form method="post" action="">
+            <label>Логин</label><br>
+            <label>
+                <input type="text" name="login" required maxlength="25">
+            </label><br>
+            <label>Пароль</label><br>
+            <label>
+                <input type="password" name="password" required maxlength="25">
+            </label><br>
+            <input type="submit" name="submit" value="Войти"><br>
+            <a href="register.php" class="button">Еще не зарегистрированы?</a><br>
+        </form>
     </div>
 </div>
 </body>
 </html>
+
+<?php
+session_start();
+require_once("mysitedb.php");
+if (isset($_POST['login'], $_POST['password'])) //проверка формы на заполнение
+{
+    $login=$_POST['login'];     //значения из формы присваиваются к переменным
+    $password=$_POST['password'];
+    $password=md5($password);   //хеширование пароля
+    $query = "SELECT * FROM users WHERE login='$login' AND password='$password';";  //поиск соответствия пользователя
+    $result = pg_query($link, $query) or die(pg_last_error($link));
+
+    if(pg_num_rows($result)==1) //если передается одна строка, то присваеваем сессии значение переменной по ключу логин
+    {
+        $_SESSION['login']=$login;
+    }
+    else
+    {
+        echo"<script> alert(\"Неверный логин или пароль.\");</script>";
+    }
+}
+if(isset($_SESSION['login']))
+{
+    $login=$_SESSION['login'];
+    echo "Здравствуйте, $login"."<br>";
+    echo "<a href='logout.php'>Выйти</a>";
+    header('Location:index2.php');
+}
+?>
